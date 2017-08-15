5b374bfe0f90f5cc4bc621b6f4900ababf65f847
ricky-clevinger <ricky.clevinger@cgi.com>
2017-08-15 15:47:09 -0400

    Update build.sh

--- src/ci/build.sh
+++ src/ci/build.sh
@@ -2,8 +2,9 @@
 set -e -u -x
 cd go-library-source
 ls -a
+export GOROOT=/usr/local/go
 export GOPATH=$PWD
-export PATH=$PATH:$GOPATH
+export PATH=$PATH:$GOPATH:$GOROOT/bin
 cd src
 ls -a
 go build main.go
