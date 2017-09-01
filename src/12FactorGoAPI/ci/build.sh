e3afc1a6780180c8c86c942c4ec55e7015edadfc
ricky-clevinger <ricky.clevinger@cgi.com>
2017-09-01 10:13:16 -0400

    Update build.sh

--- src/12FactorGoAPI/ci/build.sh
+++ src/12FactorGoAPI/ci/build.sh
@@ -3,8 +3,8 @@ set -e -u -x
 cd go-library-source
 ls -a
 export GOPATH=$PWD
-export PATH=$PATH:$GOPATH;go-library-source/src/12FactorGoAPI
+export PATH=$PATH:$GOPATH
 cd src/12FactorGoAPI
-
+ln -s /flyway/flyway.sh /usr/bin/flyway
 ls -a
 go build main.go
