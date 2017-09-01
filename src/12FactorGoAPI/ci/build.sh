3a66119b9135862bb200a5b3310af769fd688fb9
ricky-clevinger <ricky.clevinger@cgi.com>
2017-09-01 10:08:15 -0400

    Update build.sh

--- src/12FactorGoAPI/ci/build.sh
+++ src/12FactorGoAPI/ci/build.sh
@@ -3,11 +3,8 @@ set -e -u -x
 cd go-library-source
 ls -a
 export GOPATH=$PWD
-export PATH=$PATH:$GOPATH
+export PATH=$PATH:$GOPATH;go-library-source/src/12FactorGoAPI
 cd src/12FactorGoAPI
-
-sudo git clone git://github.com/pliniocsfernandes/flyway-command-line.git /opt/flyway
-sudo ln -s /opt/flyway/flyway.sh /usr/bin/flyway
 
 ls -a
 go build main.go
